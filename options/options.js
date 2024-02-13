1907f358f45d066be20774a45ea6eda92c62f94d
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-dust-dev-133406",
+  url: "https://white-dust-dev-133406.botics.co",
+  api: "https://white-dust-dev-133406.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
